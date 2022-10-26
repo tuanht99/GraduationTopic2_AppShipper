484b0405bdcca5286c0f52521beb6139c6fda5bd
tuanht99 <tuanht.02.student@fit.tdc.edu.vn>
2022-10-26 14:49:24 +0700

    Finish login with authentication

--- Freentship_Shipper/src/navigators/MainNavigator.js
+++ Freentship_Shipper/src/navigators/MainNavigator.js
@@ -1,17 +1,25 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import {HomeNavigation} from "./HomeNavigation";
-import {LocationScreen} from "../screens/LocationScreen";
+import { HomeNavigation } from './HomeNavigation';
+import { LocationScreen } from '../screens/LocationScreen';
+import { LoginScreen } from '../screens/LoginScreen'
+import { ConfirmOTP } from '../screens/ConfirmOTP'
 
 const Stack = createNativeStackNavigator()
 
 export const MainNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false}}>
+      <Stack.Navigator
+        initialRouteName='LoginScreen'
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name={'ConfirmOTP'} component={ConfirmOTP} />
         <Stack.Screen name={'LocationScreen'} component={LocationScreen} />
         <Stack.Screen name={'HomeNavigation'} component={HomeNavigation} />
+        <Stack.Screen name={'LoginScreen'} component={LoginScreen} />
       </Stack.Navigator>
-      </NavigationContainer>
+    </NavigationContainer>
   )
 }
--- Freentship_Shipper/src/screens/LocationScreen.jsx
+++ Freentship_Shipper/src/screens/LocationScreen.jsx
@@ -1,59 +1,74 @@
-import React from "react";
-import {View, Text, Button, Alert } from "react-native";
-import styles from "./LocationScreen.style";
-import * as Location from 'expo-location';
+import React from 'react'
+import { View, Text, Button, Alert, StyleSheet } from 'react-native'
+import * as Location from 'expo-location'
 
-export const LocationScreen = ({navigation}) => {
-  const [location, setLocation] = React.useState(null);
-  const [errorMsg, setErrorMsg] = React.useState(null);
-  const [isCheckedLocation, setIsCheckedLocation] = React.useState(false);
+export const LocationScreen = ({ navigation }) => {
+  const [location, setLocation] = React.useState(null)
+  const [errorMsg, setErrorMsg] = React.useState(null)
+  const [isCheckedLocation, setIsCheckedLocation] = React.useState(false)
 
   React.useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
+    ;(async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
       if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-      }else{
-        try{
-          let locationTemp = await Location.getCurrentPositionAsync({});
+        setErrorMsg('Permission to access location was denied')
+      } else {
+        try {
+          let locationTemp = await Location.getCurrentPositionAsync({})
           const locationAddress = await Location.reverseGeocodeAsync({
             latitude: locationTemp.coords.latitude,
-            longitude: locationTemp.coords.longitude
+            longitude: locationTemp.coords.longitude,
           })
           await setLocation({
             latitude: locationTemp.coords.latitude,
             longitude: locationTemp.coords.longitude,
-            address: `${locationAddress[0].streetNumber}, ${locationAddress[0].street}, ${locationAddress[0].subregion}, ${locationAddress[0].region}, ${locationAddress[0].country}`
+            address: `${locationAddress[0].streetNumber}, ${locationAddress[0].street}, ${locationAddress[0].subregion}, ${locationAddress[0].region}, ${locationAddress[0].country}`,
           })
-        }catch(e){
+        } catch (e) {
           if (!location) {
-            Alert.alert('Thông báo', 'Bạn vui lòng cho phép truy cập vị trí của bạn để sài ứng dụng', [
-              {
-                text: 'Đồng ý',
-                style: 'cancel',
-              },
-            ]);
+            Alert.alert(
+              'Thông báo',
+              'Bạn vui lòng cho phép truy cập vị trí của bạn để sử dụng ứng dụng',
+              [
+                {
+                  text: 'Đồng ý',
+                  style: 'cancel',
+                },
+              ],
+            )
           }
         }
-      }})();
-  }, [isCheckedLocation]);
+      }
+    })()
+  }, [isCheckedLocation])
 
   React.useEffect(() => {
-    if(location){
-      navigation.navigate('HomeNavigation', {location: location});
+    if (location) {
+      navigation.navigate('HomeNavigation', { location: location })
     }
   }, [location])
 
-  let text = "Vui lòng cho phép truy cập vị trí";
+  let text = 'Vui lòng cho phép truy cập vị trí'
   if (errorMsg) {
-    text = errorMsg;
+    text = errorMsg
   } else if (location) {
-    text = JSON.stringify(location);
+    text = JSON.stringify(location)
   }
   return (
     <View style={styles.container}>
       <Text>{text}</Text>
-      <Button title={'Lấy vị trí'} onPress={() => setIsCheckedLocation(!isCheckedLocation)} />
+      <Button
+        title={'Lấy vị trí'}
+        onPress={() => setIsCheckedLocation(!isCheckedLocation)}
+      />
     </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
