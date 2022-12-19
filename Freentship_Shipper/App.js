c8c277ee5731bc0315f856816f1e0fda3db8f5f5
tuanht99 <tuanht.02.student@fit.tdc.edu.vn>
2022-12-19 10:12:49 +0700

    Rebase headto

--- Freentship_Shipper/App.js
+++ Freentship_Shipper/App.js
@@ -1,13 +1,17 @@
-import * as React from 'react';
-import { MainNavigator } from "./src/navigators/MainNavigator";
 import { Provider } from 'react-redux'
 import { store } from './src/redux/store'
+import { MainNavigator } from './src/navigators/MainNavigator';
+import * as React from 'react';
+import { AppRegistry } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { name as appName } from './app.json';
 
 
 export default function App() {
     return (
-        <Provider store={store}>
-            <MainNavigator />
-        </Provider>
+        <PaperProvider>
+            <Provider store={store}>  < MainNavigator /></Provider>
+        </PaperProvider>
     );
 }
+AppRegistry.registerComponent(appName, () => App);
